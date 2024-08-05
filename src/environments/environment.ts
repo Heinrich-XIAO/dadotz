63e2d8fa5f222e4a23be6c3334c2f998dffc9538
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-08-05 14:44:44 +0000

    revert

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://purstzebyuswecdysbhh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1cnN0emVieXVzd2VjZHlzYmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI1MTQwNTksImV4cCI6MjAzODA5MDA1OX0.NuXbB_mQoZlvziGEjPczdmZ-8FayBcxaaPVQPRD1p0o',
+}
